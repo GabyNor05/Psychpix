132bd16b73fcbd1aca2f615e16b439efb991fd38
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2025-04-17 10:27:21 +0200

    Routing

--- psychpix/src/App.js
+++ psychpix/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './pages/home/components/Navbar';
+import Home from './pages/home/Home';
+import Discover from './pages/discover/Discover';
+import About from './pages/about/About';
+import Login from './pages/login/Login';
+import Cart from './pages/cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Router>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
