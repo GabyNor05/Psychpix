5d1efd39661083ec2462a84788755d7e3e625275
andrevanheerden <160416636+andrevanheerden@users.noreply.github.com>
2025-04-17 12:05:27 +0200

    login hero

--- psychpix/src/pages/login/Login.js
+++ psychpix/src/pages/login/Login.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./css/Login.css";
+import LoginHero from "./components/LoginHero";
+import LoginForm from "./components/LoginForm";
 
 
 function Login() {
   return (
-   <></>
+    <div>
+      <LoginHero />
+    </div>
+
   );
 }
 
---
+++ psychpix/src/pages/login/components/LoginHero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Login.css";
+
+function LoginHero() {
+  return (
+    <div>
+      <div className="loginHero-Cart">
+        {/* Add cart content here */}
+      </div>
+    </div>
+  );
+}
+
+export default LoginHero;
