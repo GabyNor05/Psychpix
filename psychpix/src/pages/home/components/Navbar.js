cca7b596681b09e657f8ca7a0003ceed92e02f80
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2025-04-17 11:56:00 +0200

    navbar pt 2

--- psychpix/src/pages/home/components/Navbar.js
+++ psychpix/src/pages/home/components/Navbar.js
@@ -7,9 +7,9 @@ function Navbar() {
   return (
     <nav className="navbar">
       <img className = "logo" src={Logo} alt="PyschPixels"/>
-      <div className = "nav-link" >
+      <div className = "nav-links" >
         <Link to="/" className="nav-link">Home</Link>
-        <Link to="/discover" className="nav-link">Discovr</Link>
+        <Link to="/discover" className="nav-link">Discover</Link>
         <Link to="/about" className="nav-link">About</Link>
         <Link to="/login" className="nav-link">Login</Link>
         <Link to="/cart" className="nav-link">Cart</Link>     
