353292e986df96179d899a2639b32b36d82820a4
KBOlifant <241299@virtualwindow.co.za>
2025-04-17 16:59:23 +0200

    Home page changes

--- psychpix/src/pages/home/Home.js
+++ psychpix/src/pages/home/Home.js
@@ -1,12 +1,36 @@
-import React from "react";
-
-
+import React from 'react';
+import lineSquare from './images/lil_square.png';
+import './Home.css';
 
 function Home() {
   return (
-   <>
-  
-   </>
+    <>
+      <div className='HeroSection'>
+          <div className="grid-container">
+            <div className="box" id='bentoImage1' style={{gridArea: 'box-1'}}></div>
+            <div className="box" style={{gridArea: 'box-2'}}></div>
+            <div className="box" style={{gridArea: 'box-3'}}></div>
+            <div className="box" style={{gridArea: 'box-4'}}></div>
+            <div className="box" id='bentoImage2' style={{gridArea: 'box-5'}}><div id='bentoContent1'><h4>users</h4><h1>54K</h1></div></div>
+            <div className="box" style={{gridArea: 'box-6'}}></div>
+            <div className="box" style={{gridArea: 'box-7'}}></div>
+        </div>
+      </div>
+
+      <div className="linkScroll">
+        <img className='lineSquareTL' src={lineSquare} alt='lilSquare'/>
+        <img className='lineSquareTR' src={lineSquare} alt='lilSquare'/>
+        <img className='lineSquareBL' src={lineSquare} alt='lilSquare'/>
+        <img className='lineSquareBR' src={lineSquare} alt='lilSquare'/>
+        <div><h1>link 1</h1></div>
+        <div><h1>link 2</h1></div>
+        <div><h1>link 3</h1></div>
+        <div><h1>link 4</h1></div>
+        <div><h1>link 5</h1></div>
+        <div><h1>link 6</h1></div>
+        <div><h1>link 7</h1></div>
+      </div>
+    </>
   );
 }
 
--- psychpix/src/pages/home/components/Navbar.js
+++ psychpix/src/pages/home/components/Navbar.js
@@ -6,13 +6,14 @@ import Logo from "../../logo.png";
 function Navbar() {
   return (
     <nav className="navbar">
-      <img className = "logo" src={Logo} alt="PyschPixels"/>
+      <div className="navlinks">
+        <img className="logo" src={Logo} alt="PyschPixels"/>
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/discover" className="nav-link">Discover</Link>
         <Link to="/about" className="nav-link">About</Link>
         <Link to="/login" className="nav-link">Login</Link>
-        <Link to="/cart" className="nav-link">Cart</Link>     
-      </div>
+        <Link to="/cart" className="nav-link">Cart</Link>  
+      </div>    
     </nav>
   );
 }
